16046ab08ce452bec3c23ba790bc806d51231e4d
Cesar Lopez <jcesarln199902mail.com>
2026-09-23 18:35:45 -0600

    napoles

---
+++ Js/script.js
@@ -0,0 +1,8 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const menuNav = document.querySelector(".Menu-nav");
+
+menuToggle.addEventListener("click", () => {
+
+    menuNav.classList.toggle("active");
+
+});
